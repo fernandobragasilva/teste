10ec74afc9700db730b15210e837278b55e7b717
Fernando Braga <fernando.braga@ceviu.net>
2016-08-11 17:39:43 -0300

    teste para falhar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
     public function index(){
-    return view('home');
+     return view('home');
     }
 }
+?>

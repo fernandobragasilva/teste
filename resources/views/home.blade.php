ddf38d1a50b6a0c4dc6c5227090536d92bad60db
Fernando Braga <fernando.braga@ceviu.net>
2016-08-11 12:59:42 -0300

    adicionando home controller com exemplo de layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.padrao')
+@section('content')
+<div class="container conteudo">
+    <div class="container">&nbsp;</div>
+    <div class="container">&nbsp;</div>
+    <div class="container">&nbsp;</div>
+    <div class="container">&nbsp;</div>
+    <div class="container">&nbsp;</div>
+    <div class="page-header">
+        <h1>Sticky footer with fixed navbar</h1>
+    </div>
+    <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+    <p>Back to <a href="#">the default sticky footer</a> minus the navbar.</p>
+</div>
+@endsection

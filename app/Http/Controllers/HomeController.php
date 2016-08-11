7db728edd6040ffe7ad7a5b534dd884dea8be9d5
Fernando Braga <fernando.braga@ceviu.net>
2016-08-11 15:18:28 -0300

    teste mal formatado.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,9 @@
-<?php
-
+<?php 
 namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+    return view('home');
     }
 }

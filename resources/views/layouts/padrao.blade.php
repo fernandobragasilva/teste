be16840d362c45e58ea9c068ed89c5453811cb50
Fernando Braga <fernando.braga@ceviu.net>
2016-08-12 10:04:14 -0300

    adicionando controller e layout de teste

---
+++ resources/views/layouts/padrao.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Projeto de teste</title>
+
+        <!-- Favicon -->
+        <link href="/img/icon.png" rel="shortcut icon" />
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+
+        @yield('styles-plugins')
+        <link href="/css/main.css" rel="stylesheet">
+        @yield('styles')
+    </head>
+    <body cz-shortcut-listen="true">
+        @include('partials.cabecalho')
+
+        @yield('content')
+
+        @include('partials.rodape')
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+        @yield('scripts-plugins')
+        <script src="/js/main.js"></script>
+        @yield('scripts')
+    </body>
+</html>

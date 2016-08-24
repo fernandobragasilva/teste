2b099d1b518d2e965dc25f329f6211a4780ed9eb
Fernando Braga <fernando.braga@ceviu.net>
2016-08-24 14:18:20 -0300

    teste quarta-feira travis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $codigo = password_hash("rasmuslerdorf", PASSWORD_DEFAULT);
-        return view('home')->with("teste", $codigo);
+        return view('home');
     }
 }
